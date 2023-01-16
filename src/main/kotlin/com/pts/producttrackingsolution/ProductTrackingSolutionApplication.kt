fb93beaf6c2e1d4f75a180738c964f41f7c1dbd0
안정후 <austinan@naver.com>
2023-01-16 23:19:32 +0900

    git init

---
+++ src/main/kotlin/com/pts/producttrackingsolution/ProductTrackingSolutionApplication.kt
@@ -0,0 +1,11 @@
+package com.pts.producttrackingsolution
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductTrackingSolutionApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductTrackingSolutionApplication>(*args)
+}
---
+++ src/test/kotlin/com/pts/producttrackingsolution/ProductTrackingSolutionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pts.producttrackingsolution
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductTrackingSolutionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
